4a5515bbc6337777f6ec10ecaaac149df65a1ea6
Max <crono0720@gmail.com>
2018-05-03 10:33:08 +0800

    add new post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,23 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:notice] = "Successfully saved"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = @post.errors.full_messages.to_sentence if @post.errors.any?
+      @posts = Post.all
+      render :index
+    end
+  end
+
   def show
   end
 
@@ -15,6 +32,16 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title,:description)
   end
 
 end
