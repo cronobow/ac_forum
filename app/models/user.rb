7270ed74da1d6385fe7b25f75cb93a57520f91dc
Max <crono0720@gmail.com>
2018-05-05 22:53:30 +0800

    update layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   has_many :replies
+
+  def admin?
+    self.role == "admin"
+  end
 end
