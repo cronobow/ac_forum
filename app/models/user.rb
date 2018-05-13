bbd0ec08c3ae8576d1a1fe5331deb7e9f9808bf2
Max <crono0720@gmail.com>
2018-05-13 21:32:40 +0800

    add friend relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   has_many :replies
   has_many :collects
   has_many :collect_posts, through: :collects, source: :post
+  has_many :friendships
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :frienders, through: :inverse_friendships, source: :user
+
 
   def admin?
     self.role == "admin"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510093235) do
+ActiveRecord::Schema.define(version: 20180511090322) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20180510093235) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_collects_on_post_id"
     t.index ["user_id"], name: "index_collects_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id"
   end
 
   create_table "post_categoryships", force: :cascade do |t|
