6dcb4b2347592c989a499ccf1cee4c308c63916a
Max <crono0720@gmail.com>
2018-05-14 14:05:05 +0800

    add update replies

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -9,16 +9,28 @@ class RepliesController < ApplicationController
     else
       @reply = @post.replies.build(reply_params)
       @reply.user = current_user
-      @reply.save
+      if @reply.save
+        flash[:notice] = '留言已發表'
+      else
+        flash[:alert] = @reply.errors.full_messages.to_sentence if @reply.errors.any?
+      end
     end
     redirect_to post_path(@post)
   end
 
   def edit
+    respond_to do |format|
+      format.js
+    end
   end
 
   def update
-
+    if @reply.update(reply_params)
+      flash[:notice] = '留言已更新'
+    else
+      flash[:alert] = @reply.errors.full_messages.to_sentence if @reply.errors.any?
+    end
+    redirect_to post_path(@post)
   end
 
   def destroy
